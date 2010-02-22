0c6f8bece3e8030fbeac8443e85327783d2102fc
Kyle Slattery <kyle@kyleslattery.com>
2010-02-21 21:21:48 -0500

    Add spec for Webify::File

---
+++ spec/file_spec.rb
@@ -0,0 +1,25 @@
+$LOAD_PATH.unshift File.dirname(__FILE__) + "/../lib"
+require 'webify'
+
+describe Webify::File, '.new' do
+  before(:each) do
+    @mock_file = mock(File, {:close => nil})
+    File.stub!(:new).and_return(@mock_file)
+  end
+  
+  it "should not return an error" do
+    lambda {
+      Webify::File.new("fake.file", "w") {hello = "asdf"}
+    }.should_not raise_exception
+  end
+  
+  it "should call the block" do
+    block_called = false
+    
+    Webify::File.new("fake.file", "w") do
+      block_called = true
+    end
+    
+    block_called.should be_true
+  end
+end
