8e566e74e1cd2a4e23a50820f952eed2072a0031
Kyle Slattery <kyle@kyleslattery.com>
2010-02-21 22:53:12 -0500

    Fill out some more of Webify::Builder

--- lib/webify/builder.rb
+++ lib/webify/builder.rb
@@ -1,12 +1,20 @@
 module Webify
   class Builder
-    attr_accessor :source_dir
+    attr_accessor :source_dir, :output_dir
+    
+    def initialize(source=nil)
+      @source_dir = source || "./"
+    end
         
-    def source_files
+    def source_paths
       dir  = @source_dir.nil? ? "./" : @source_dir
       glob = File.join(dir, "**", "*.md")
       
       Dir[glob]
+    end
+    
+    def output_path_for(source_path)
+      source_path.gsub(@source_dir, @output_dir).gsub(/\.md$/, ".htm")
     end
   end
 end
--- spec/builder_spec.rb
+++ spec/builder_spec.rb
@@ -10,9 +10,26 @@ describe Webify::Builder, "attributes" do
     @builder.source_dir = "asdf"
     @builder.source_dir.should == "asdf"
   end
+  
+  it "should allow output_dir to be set" do
+    @builder.output_dir = "asdf"
+    @builder.output_dir.should == "asdf"
+  end
 end
 
-describe Webify::Builder, "#source_files" do  
+describe Webify::Builder, ".new" do
+  it "should set source_dir if specified" do
+    @builder = Webify::Builder.new("directory")
+    @builder.source_dir.should == "directory"
+  end
+  
+  it "should set source_dir to ./ if not specified" do
+    @builder = Webify::Builder.new
+    @builder.source_dir.should == "./"
+  end
+end
+
+describe Webify::Builder, "#source_paths" do  
   before(:each) do
     @builder = Webify::Builder.new
   end
@@ -29,10 +46,22 @@ describe Webify::Builder, "#source_files" do
   
   it "should return result of Dir[]" do
     Dir.stub!(:[]).and_return(["asdf"])
-    @builder.source_files.should == ["asdf"]
+    @builder.source_paths.should == ["asdf"]
   end
   
   after(:each) do
-    @builder.source_files
+    @builder.source_paths
+  end
+end
+
+describe Webify::Builder, "#output_path_for" do
+  before(:each) do
+    @builder = Webify::Builder.new
+    @builder.source_dir = "./"
+    @builder.output_dir = "../output/"
+  end
+  
+  it "should return file with proper path and extension" do
+    @builder.output_path_for(File.join("./", "directory/" "asdf.md")).should == File.join("../output/", "directory/", "asdf.htm")
   end
 end
